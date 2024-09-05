cbe36f051266e0cf5e85a45f004b19e206ee7724
Letus252 <RandomF0x@proton.me>
2024-09-05 12:35:48 +0200

    Removed dmenu config

--- config.h
+++ config.h
@@ -31,8 +31,6 @@ static const Rule rules[] = {
 	{ "Firefox",           NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
 	{ "st",                NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ NULL,                NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
-    { "SlayTheSpire",      NULL,     NULL,           0,         0,          0,           1,        -1 }, /* game */
-
 };
 
 /* layout(s) */
@@ -61,7 +59,8 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+//static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", NULL};
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
